5e8fbd44f9505cb84696d1e197142eff8b0c93b3
Arina Iljinih <iljinih.ar@yandex.ru>
2022-02-14 13:32:47 +0700

    картинка клик

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-let s = 2000000
-let p = 10
-let year = 5
+let hide = true
 
-let pm = (p/100)/12
-let km = year*12
-let ep = s *pm*Math.pow((1+pm),km)/(Math.pow((1+pm),km)-1)
-let perep = km*ep-s
-console.log(perep)
+function sayHello(){
+    if (hide == true){
+        document.getElementById('kartinka').src = 'bg.png'
+        hide = false
+    }
+    else{
+        document.getElementById('kartinka').src = ''
+        hide = true
+    }
+}
