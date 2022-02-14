f247dea55ae347370502e6c25f860cb4326b02a1
Arina Iljinih <iljinih.ar@yandex.ru>
2022-02-14 12:33:41 +0700

    площадь прямоугольника

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-let a = 2;
-let b = 1;
-let c = -2;
-let x1, x2;
-
-let D = b*b-4*a*c;
-
-if (D > 0){
-    x1 = (-b+Math.sqrt(D))/(2*a);
-    x2 = (-b-Math.sqrt(D))/(2*a);
-    console.log(`x1 = ${x1}`);
-    console.log(`x2 = ${x2}`);
-}
-else if (D = 0){
-    x1 = -b / (2*a);
-    console.log('x1 = ${x1}');
-}
-else{
-    console.log('действительных корней нет');
-}
+let height = 23
+let width = 10
+let s = height * width
+console.log(s)
