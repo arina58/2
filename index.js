6bb10aa77ee91bd43f16f934aa8117198f8c2ea7
Arina Iljinih <iljinih.ar@yandex.ru>
2022-02-21 09:22:18 +0700

    джава ученики в таблице

--- index.js
+++ index.js
@@ -5,12 +5,32 @@ let students = [
     {id: 4, name: 'Георгий', surname:'Бочкарев'},
     {id: 4, name: 'Матвей', surname:'Гаврилов'}
 ]
-for (let i = 0; i<students.length; i++){
-    let id = students[i].id
-    let name = students[i].name
-    let surname = students[i].surname
-    console.log('Номер: ' + id)
-    console.log('Имя: ' + name)
-    console.log('Фамилия: ' + surname)
-    console.log('-----------------------')
+
+function load_oll(){
+    let table = document.getElementById('tbl_all')
+    for (let i = 0; i<students.length; i++){
+        let id = students[i].id
+        let name = students[i].name
+        let surname = students[i].surname
+        console.log('Номер: ' + id)
+        console.log('Имя: ' + name)
+        console.log('Фамилия: ' + surname)
+        // создадим строку и три ячейки
+        let tr = document.createElement('tr')
+        let td1 = document.createElement('td')
+        let td2 = document.createElement('td')
+        let td3 = document.createElement('td')
+        //запишем данные в ячейки
+        td1.textContent = id
+        td2.textContent = name
+        td3.textContent = surname
+        //вставим ячейки в строку
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        //вставим строку в таблицу
+        table.appendChild(tr)
+
+    }
 }
+
