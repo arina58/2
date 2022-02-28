13f6496940e5b3885fd62c248943aebf49b6aba1
Arina Iljinih <iljinih.ar@yandex.ru>
2022-02-28 13:24:03 +0700

    готова страница подробной информации

---
+++ js/index.js
@@ -0,0 +1,71 @@
+
+let students = []
+let id_current = 0
+
+//загружвем информацию с сайта
+function load_from_site() {
+    $.get('http://217.71.129.139:4035/students.php', function(data){
+        students = JSON.parse(data)['response'] 
+    });
+}
+
+function load_oll() {
+    load_from_site()
+    let table = document.getElementById('tbl_all')
+    for (let i = 0; i < students.length; i++) {
+    let id = students[i].id
+    let name = students[i].name
+    let surname = students[i].surname
+    
+    let tr = document.createElement('tr')
+    let td1 = document.createElement('td')
+    let td2 = document.createElement('td')
+    let td3 = document.createElement('td')
+    let td4 = document.createElement('td')
+    
+    td1.textContent = id
+    td2.textContent = name
+    td3.textContent = surname
+    
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    
+    table.appendChild(tr)
+    }
+}
+    
+ 
+
+function load_student(id) {
+    
+    let head = $('#zagolovok')
+    head.prop('textContent',  'Информация о студенте № ' + students[id].id)
+    
+    $("#name") .prop('textContent', students[id].name)
+    $("#surname") .prop('textContent', students[id].surname)
+    $("#scores") .prop('textContent', students[id].scores)
+    
+    let ball = Array.from(students[id].scores).reduce((a, b) => (a + b)) / 5
+    $("#average").prop ('textContent', ball)
+    $("#logo").prop('src', 'http://217.71.129.139:4035/' + students[id].logo)  
+}
+   
+function next() {
+    id_current++
+    if (id_current > 0)  {$("#btn_prev").prop('disabled', false)}
+    if (id_current === students.length-1) {
+    $("#btn_next").prop('disabled', true) 
+    }
+    
+    load_student(id_current)
+}
+    
+function prev() {
+    id_current--
+    if (id_current === 0) {$("#btn_prev").prop('disabled', true)}
+    if (id_current !== students.length-1) {$("#btn_next").prop('disabled', false)}
+
+    load_student(id_current)
+}
