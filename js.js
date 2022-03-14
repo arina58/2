0df66bb4b0830695c8429a5cad7828691d66783b
Arina Iljinih <iljinih.ar@yandex.ru>
2022-03-14 11:13:24 +0700

    Возвращение на место

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {  
-    $("#head1").draggable({axis:"x"});
-    $("#head2").draggable({axis:"y"});
-
     $("#head1").draggable({cursor:"move"});
     $("#head2").draggable({cursor:"help"}); 
 
     $("#head1").draggable({helper:"clone"});
     $("#head2").draggable({helper:function(event){return $("<div>Я элемент помощник.</div>")}});
+
+    $("#head1").draggable({revert:true});
  });
