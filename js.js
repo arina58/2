1686c35479676a0631664aee4263ae54c1478595
Arina Iljinih <iljinih.ar@yandex.ru>
2022-03-14 11:22:38 +0700

    drop and over

--- js.js
+++ js.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {  
-    $("#head1").draggable({cursor:"move"});
-    $("#head2").draggable({cursor:"help"}); 
-
-    $("#head1").draggable({helper:"clone"});
-    $("#head2").draggable({helper:function(event){return $("<div>Я элемент помощник.</div>")}});
-
-    $("#head1").draggable({revert:true});
+    $("#drag1").draggable({stack:"#drop1"});
+    $("#drop1").droppable({
+       over:function(){
+          $("#drag1").css("background-color","#d7fa99");
+          $("#drop1").css("background-color","#d7fa99");
+       },
+       drop:function(){
+          $("#drag1").css("display","none");
+          $("#drop1").css("background-color","#c4f66f");
+          $("#drop1").html("Перемещение завершено успешно.");
+          alert("Вы перетащили элемент с id=drag1 в область с id=drop1.");
+       }
+    });
  });
