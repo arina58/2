8191c938b4c05d2828e8b9174880819c06a3af43
Arina Iljinih <iljinih.ar@yandex.ru>
2022-03-14 11:27:46 +0700

    Растягивание

--- js.js
+++ js.js
@@ -1,16 +1,3 @@
 $(document).ready(function() {  
-    $("#drag1").draggable({stack:"#drop1"});
-    $("#drag2").draggable({stack:"#drop1",revert:true});
-    $("#drop1").droppable({
-       accept:"#drag1",
-       over:function(){
-          $("#drag1").css("background-color","#d7fa99");
-          $("#drop1").css("background-color","#d7fa99");
-       },
-       drop:function(){
-          $("#drag1").css("display","none");
-          $("#drop1").css("background-color","#c4f66f");
-          $("#drop1").html("Перемещение завершено успешно.");
-       }
-    });
+    $("#drag1").resizable();
  });
