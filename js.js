6decd7cefb3b841895da6c8b577d8a8cc1447dce
Arina Iljinih <iljinih.ar@yandex.ru>
2022-03-14 11:24:56 +0700

    accept

--- js.js
+++ js.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {  
     $("#drag1").draggable({stack:"#drop1"});
+    $("#drag2").draggable({stack:"#drop1",revert:true});
     $("#drop1").droppable({
+       accept:"#drag1",
        over:function(){
           $("#drag1").css("background-color","#d7fa99");
           $("#drop1").css("background-color","#d7fa99");
@@ -9,7 +11,6 @@ $(document).ready(function() {
           $("#drag1").css("display","none");
           $("#drop1").css("background-color","#c4f66f");
           $("#drop1").html("Перемещение завершено успешно.");
-          alert("Вы перетащили элемент с id=drag1 в область с id=drop1.");
        }
     });
  });
