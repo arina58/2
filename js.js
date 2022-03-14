b4af6259d01c86cedfe21ddc18a47319eaa55f41
Arina Iljinih <iljinih.ar@yandex.ru>
2022-03-14 11:11:12 +0700

    helper

--- js.js
+++ js.js
@@ -4,4 +4,7 @@ $(document).ready(function() {
 
     $("#head1").draggable({cursor:"move"});
     $("#head2").draggable({cursor:"help"}); 
+
+    $("#head1").draggable({helper:"clone"});
+    $("#head2").draggable({helper:function(event){return $("<div>Я элемент помощник.</div>")}});
  });
