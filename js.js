1dd2df6440f225706bbc6084670623a56b4fb716
Arina Iljinih <iljinih.ar@yandex.ru>
2022-03-14 11:07:16 +0700

    курсор

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
-$(document).ready(function() {
-  
+$(document).ready(function() {  
     $("#head1").draggable({axis:"x"});
     $("#head2").draggable({axis:"y"});
- 
+
+    $("#head1").draggable({cursor:"move"});
+    $("#head2").draggable({cursor:"help"}); 
  });
