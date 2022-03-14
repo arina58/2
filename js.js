bd749c7ce3c7d4f1cdbaf311647f28cada997fb3
Arina Iljinih <iljinih.ar@yandex.ru>
2022-03-14 13:22:49 +0700

    задание1

--- js.js
+++ js.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {  
-    $("#sort1,#sort2").sortable({connectWith:".connect"});
+    $("#el1").draggable();
+    $("#el1").resizable();
+
+    $("#sort1").sortable();
+
+    $("#el2").draggable();
+    $("#el3").draggable();
+    $("#drop1").droppable({
+        accept:"#el2, #el3",
+        over:function(event, ui){
+            let el = ui.draggable
+            el.css("background-color","#c4f66f");
+        },
+        drop:function(event, ui){
+           let el = ui.draggable
+           el.hide()
+        }
+        });
+     $("#drop2").droppable({
+        accept:"#el3",
+        drop:function(){
+           $("#el3").css("display","none");
+           $("#drop2").css("background-color","#c4f66f");
+        }
+     });
  });
